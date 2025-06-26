a9d78a900a79f00fc857deb9c7af9e27b88eed22
董立 <caocong1@gmail.com>
2025-06-26 19:08:22 +0800

    feat: 修复主窗口变量作用域问题，确保正确创建浏览器窗口

--- src/index.cjs
+++ src/index.cjs
@@ -15,9 +15,11 @@ if (require('electron-squirrel-startup')) {
   app.quit();
 }
 
+let mainWindow;
+
 const createWindow = () => {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 1400,
     height: 900,
     webPreferences: {
